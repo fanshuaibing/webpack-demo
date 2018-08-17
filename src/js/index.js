39965578e7c175e0e63625b203d5c3fec2a789a7
fatfan <fatfan1020@gmail.com>
2018-08-17 21:37:59 +0800

    webpack初试

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import x from './model1'
+import y from './model2'
+import "../css/main.scss"
+x()
+let button = document.querySelector("input")
+button.addEventListener("click",function () {
+	alert("点击了按钮")
+})
+y()
